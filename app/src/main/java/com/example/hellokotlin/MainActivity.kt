a0ff05fbebab87c360667f6e6911c45d834ec0b2
Maria <veromii06@gmail.com>
2022-06-05 16:17:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private var tts: TextToSpeech? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+
+        findViewById<Button>(R.id.btPlay).setOnClickListener{speak()}
+    }
+    private fun speak(){
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
+
+        if (message.isEmpty()){
+           /* findViewById<TextView>(R.id.tvStatus).text = "Introduce un texto!"*/
+            message = "Debes de introducir un texto"
+        }
+
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_active)
+            tts!!.language = Locale("ES")
+        } else {
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_no_active)
+        }
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
